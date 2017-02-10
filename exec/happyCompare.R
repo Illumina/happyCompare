e48676ad6e743dce2ebe55f7725ccb612b31f25f
Mar Gonzalez-Porta <mgonzalez@illumina.com>
2017-02-10 15:48:58 +0000

    generating example report with Rscript

---
+++ exec/happyCompare.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+## parse args
+option_list = list(
+    optparse::make_option(c("-i", "--input_template"), type = "character", default = NULL, 
+                help = "Path to input template Rmd."),
+    optparse::make_option(c("-c", "--config"), type = "character", default = NULL,
+                help = "Path to a happyCompare config."),
+    optparse::make_option(c("-o", "--output_dir"), type = "character", default = NULL, 
+                help = "Path to output directory.")
+)
+opt_parser = optparse::OptionParser(option_list = option_list)
+opt = optparse::parse_args(opt_parser)
+if (any(sapply(opt, is.null))) {
+    optparse::print_help(opt_parser)
+    stop("Missing required arguments", call. = FALSE)
+}
+
+## generate report
+rmarkdown::render(input = opt$input_template, 
+                  output_dir = opt$output_dir, 
+                  params = list(config = opt$config))
+message(sprintf("\n[DONE] Output: %s", opt$output_dir))
