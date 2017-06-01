bdfde36c4c5d7a7bd482d0b02327d338878a4037
Mar Gonzalez-Porta <mgonzalez@illumina.com>
2017-06-01 18:12:54 +0100

    re-arranging tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,3 +3,4 @@ library(happyCompare)
 
 test_check("happyCompare")
 load(system.file("demo_data/happyCompare_list.RData", package = "happyCompare"))
+load(system.file("demo_data/happy_extended.RData", package = "happyCompare"))
---
+++ tests/testthat/test_io.R
@@ -0,0 +1,2 @@
+## add tests for I/O
+## TODO: needs a small enough data set
